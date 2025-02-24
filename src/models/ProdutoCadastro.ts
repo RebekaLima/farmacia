5c3764016f84c122d139cfa89d1552309b7c61a7
Rebeka <rebeka.silva.emailprofissional@gmail.com>
2025-02-24 16:22:34 -0300

    Construindo Crud

---
+++ src/models/ProdutoCadastro.ts
@@ -0,0 +1,9 @@
+export default interface ProdutoCadastro {
+    id: number;
+  nome: string;
+  descricao: string;
+  quantidade: number;
+  laboratorio: string;
+  preco: number;
+  foto: string;
+}
